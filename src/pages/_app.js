9a9222764bbf129329ace34f9570f028cc8461dd
Michael Markey <michaelamarkey@gmail.com>
2020-06-02 14:33:36 +0900

    add material ui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,28 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import { ApolloProvider } from '@apollo/client'
 import client from 'src/graphql/client'
 
 function App ({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.getElementById('jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>Kakeibo</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ApolloProvider client={client}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
   )
 }
 
