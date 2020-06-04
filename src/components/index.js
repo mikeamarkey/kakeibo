c83f36d40e74677e8808a0fadc08afd4360b3482
Michael Markey <michaelamarkey@gmail.com>
2020-06-04 17:16:31 +0900

    refactor dialog

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Footer from './Footer'
 import Header from './Header'
 import Price from './Price'
+import TransactionDialog from './TransactionDialog'
 import TransactionsList from './TransactionsList'
 
-export { Footer, Header, Price, TransactionsList }
+export { Footer, Header, Price, TransactionDialog, TransactionsList }
