9f9544d45c84d858eb6ef4951756ba4e884bd9d9
Michael Markey <michaelamarkey@gmail.com>
2020-06-05 13:04:51 +0900

    add grouping of transactions into days

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -6,6 +6,13 @@ const useStyles = makeStyles((theme) => ({
   root: {
     overflowY: 'auto'
   },
+  subheader: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: theme.spacing(1),
+    padding: theme.spacing(1)
+  },
   card: {
     margin: theme.spacing(1),
     padding: theme.spacing(1)
@@ -26,46 +33,70 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const sortTransactions = (a, b) => {
-  if (moment(a.date).isBefore(moment(b.date))) {
+const sortTransactionGroups = (a, b) => {
+  if (moment(a).isBefore(moment(b))) {
     return 1
   } else {
     return -1
   }
 }
 
+const sortTransactions = (a, b) => b._ts - a._ts
+
 const TransactionsList = ({ transactions, setDialogContent }) => {
   const css = useStyles()
+  const transactionGroups = transactions.reduce((acc, item) => {
+    if (!acc.groups[item.date]) {
+      acc.groups[item.date] = {
+        total: item.price,
+        transactions: [item]
+      }
+    } else {
+      acc.groups[item.date].total += item.price
+      acc.groups[item.date].transactions.push(item)
+    }
+    acc.total += item.price
+    return acc
+  }, { total: 0, groups: {} })
 
   return (
     <div className={css.root}>
-      {transactions.slice().sort(sortTransactions).map((transaction) => (
-        <Paper
-          key={transaction.id}
-          variant='outlined'
-          className={css.card}
-          onClick={() => {
-            setDialogContent({ ...transaction, category: transaction.category._id })
-          }}
-        >
-          <div className={css.content}>
-            <div>
-              {transaction.category && (
-                <Typography>{transaction.category.name}</Typography>
-              )}
-              {transaction.note.length > 0 && (
-                <Typography className={css.small} variant='caption'>{transaction.note}</Typography>
-              )}
-            </div>
+      {Object.keys(transactionGroups.groups).sort(sortTransactionGroups).map((date) => (
+        <div key={date}>
+          <div className={css.subheader}>
+            <Typography variant='subtitle1'>
+              {moment(date).format('M/D')}
+            </Typography>
 
-            <div className={css.right}>
-              <Typography>
-                <Price price={transaction.price} />
-              </Typography>
-              <Typography className={css.small}>{moment(transaction.date).format('M/D')}</Typography>
-            </div>
+            <Typography variant='subtitle1'>
+              <Price price={transactionGroups.groups[date].total} />
+            </Typography>
           </div>
-        </Paper>
+
+          {transactionGroups.groups[date].transactions.sort(sortTransactions).map((transaction) => (
+            <Paper
+              key={transaction.id}
+              variant='outlined'
+              className={css.card}
+              onClick={() => {
+                setDialogContent({ ...transaction, category: transaction.category._id })
+              }}
+            >
+              <div className={css.content}>
+                <div>
+                  <Typography variant='body2'>{transaction.category.name}</Typography>
+                  {transaction.note.length > 0 && (
+                    <Typography className={css.small} variant='caption'>{transaction.note}</Typography>
+                  )}
+                </div>
+
+                <Typography variant='body2'>
+                  <Price price={transaction.price} />
+                </Typography>
+              </div>
+            </Paper>
+          ))}
+        </div>
       ))}
     </div>
   )
