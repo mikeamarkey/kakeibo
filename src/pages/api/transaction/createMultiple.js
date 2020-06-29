eadad9d80903bea8e35ac33c74aa4846fee65f66
Michael Markey <michaelamarkey@gmail.com>
2020-06-29 12:10:24 +0900

    fix cache updating, add loading indicator (#10)

--- src/pages/api/transaction/createMultiple.js
+++ src/pages/api/transaction/createMultiple.js
@@ -1,6 +1,6 @@
 import { createMultiple } from 'src/graphql/faunadb'
 
-const Sort = async (req, res) => {
+const CreateMultiple = async (req, res) => {
   if (!req.method === 'POST') {
     res.setHeader('Allow', 'POST')
     return res.status(405).end('Method not allowed')
@@ -8,7 +8,18 @@ const Sort = async (req, res) => {
 
   const data = await JSON.parse(req.body)
   const result = await createMultiple(data)
-  res.json(result)
+  const formatted = result.map((item) => {
+    return {
+      _id: item.ref.id,
+      _ts: item.ts,
+      name: null,
+      order: null,
+      ...item.data,
+      category: item.data.category ? item.data.category.id : null,
+      date: item.data.date ? item.data.date.toJSON()['@date'] : null
+    }
+  })
+  res.json(formatted)
 }
 
-export default Sort
+export default CreateMultiple
