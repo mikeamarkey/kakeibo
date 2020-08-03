1de7175fe66d243439dc2ecce6b6669fa90033c9
Michael Markey <mike@bivalabo.co.jp>
2020-08-03 17:36:01 +0900

    fix viewport meta header issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,10 +8,6 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta charSet='utf-8' />
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no'
-          />
           <meta name='description' content='Kakeibo - Family Accounting App' />
           <link rel='shortcut icon' href='/favicon.ico' />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
